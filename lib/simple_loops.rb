f608c0cfd01b519ce360dbae860a398ad41e4dce
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-18 05:32:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,12 @@ def
       count += 1 
     end
   end
+  
+  def 
+    loop_message_n_times(message,limit)
+    count = 0 
+    while count < limit do
+      puts message
+      count +=1 
+    end
+  end
