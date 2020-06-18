8cc99045e49e0eb362847908275ce152931a6b87
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-18 05:35:26 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,10 +17,9 @@ def
   end
   
   def output_array(array)
-    count = 0 
-    while count < array.length 
-    do
-        puts array[count]
-        count += 1 
-      end 
-    end
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
+end
